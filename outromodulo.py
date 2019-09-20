cc827d01972832e046f91ef1f982a48b66d368e5
AE <alinem2104@gmail.com>
2019-09-20 12:08:41 -0300

    continuação teoria

--- outromodulo.py
+++ outromodulo.py
@@ -24,4 +24,23 @@
 # cd <nome do diretorio>
 # python -m venv .venv --> cria virtualenv
 # source .venv/Scripts/activate --> ativar
+    # echo "django" >> requirements.txt
 # pip install -r requirements.txt --> instalar o django que esta no arquivo txt
+
+# django-admin startproject salada .  --> iniciar o django, criar um projeto django 
+# python manage.py startapp tempero --> criar uma aplicação django
+
+
+# MVC
+# M = model - cria as coisas que vão ser salvas no banco de dados (SGBG - Sistema gerenciador de banco de dados que realmente é o banco de dados)
+# V = view - aonde são construidas as páginas (regra de negócio)
+# C - controlls - lógica de negocio
+# MTV - tudo dentro de tempero
+# M - model - comunica com o banco de dados (vai estar na = models.py)
+# T - templates - páginas (html) (vai estar na = templates/)
+# V - view - lógico de negócio (vai estar na = views.py)
+
+
+## proximos passos git
+# python manage.py migrate
+# python manage.py runserver
